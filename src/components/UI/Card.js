a9ebaea47f795e2309defa3dd8e036d6f1c5e6d8
SuryaN97 <94628780+SuryaN97@users.noreply.github.com>
2023-04-08 17:29:48 +0530

    commit

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import classes from './Card.module.css';
 
+//function Card (props) {
+ // return <div className={'${classes.card} ${props.className}'}>{props.childre}</div>
+//}//
+
 const Card = (props) => {
   return <div className={`${classes.card} ${props.className}`}>{props.children}</div>;
 };
